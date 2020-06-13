fd581fe2fe290c891915f3b17f2ab8e495cc8b5f
Adrian <adriancias.dev@gmail.com>
2020-06-13 20:30:31 +0200

    first repo

---
+++ js/main.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function () {
+    $(".intro").ripples({
+        dropRadius: 100,
+        perturbance: 0.005,
+    });
+
+    var typed = new Typed('.intro_text', {
+        strings: ["<span class='txtcolor_1'>coding</span>", "<span class='txtcolor_2'>designing</span>", "<span class='txtcolor_3'>supporting</span>"],
+        typeSpeed: 150,
+        loop: true,
+
+    });
+
+    $(window).scroll(function () {
+
+        var top = $(window).scrollTop();
+
+        if (top >= 60) {
+            $("nav").addClass('navabar_background');
+        } else
+            if ($("nav").hasClass('navabar_background')) {
+                $("nav").removeClass('navabar_background');
+            }
+    })
+
+    $('.projects').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true
+        }
+    });
+
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+});
+
